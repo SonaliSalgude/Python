9f9fca3ce8de03a2a5e156de9db858294e6a8efa
SonaliSalgude <141991930+SonaliSalgude@users.noreply.github.com>
2023-10-27 12:10:34 +0530

    Add files via upload

---
+++ Company.py
@@ -0,0 +1,12 @@
+class Company:
+    def __init__(self, name, company):
+        self.name = name
+        self.company = company
+ 
+    def show(self):
+        print("Hello my name is " + self.name+" and I" +
+              " work in "+self.company+".")
+ 
+ 
+obj = Company("John", "GeeksForGeeks")
+obj.show()
